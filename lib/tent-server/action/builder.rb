1c97066ee8073bae7c10751d12afa24091060ebb
Jesse Stuart <jessestuart@gmail.com>
2012-08-23 13:11:41 -0400

    Add Tent::Server::Action::Builder

---
+++ lib/tent-server/action/builder.rb
@@ -0,0 +1,42 @@
+module Tent
+  module Server
+    module Action
+      # Tent::Server::Action::Builder implements a small DSL to add middleware to the call stack.
+      #
+      # Example:
+      #
+      #  app = Tent::Server::Action::Builder.new.tap do |b|
+      #    b.use Rack::CommonLogger
+      #  end
+      #
+      #  app.call(env)
+
+      class Builder
+        # Specifies middleware to use in a stack.
+        #
+        #   class Middleware
+        #     def initialize(app)
+        #       @app = app
+        #     end
+        #
+        #     def call(env)
+        #       @app.call(env)
+        #     end
+        #   end
+        #
+        def use(middleware, *args, &block)
+          (@stack ||= []) << proc { |app| middleware.new(app, *args, &block) }
+        end
+
+        def to_app
+          app = lambda { |env| env }
+          @stack.reverse.inject(app) { |a,e| e[a] }
+        end
+
+        def call(env)
+          to_app.call(env)
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/tent-server/action/builder_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path("../../../../../lib/tent-server/action/builder", __FILE__)
+
+class TestMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    env[:test] ||= 0
+    env[:test] += 1
+    @app.call(env)
+  end
+end
+
+describe Tent::Server::Action::Builder do
+  let(:instance) { described_class.new }
+
+  context "basic `use`" do
+    it "should add items to the stack" do
+      instance.use TestMiddleware
+      env = instance.call({})
+      expect(env[:test]).to eq(1)
+    end
+
+    it "should add multiple items to the stack" do
+      instance.use TestMiddleware
+      instance.use TestMiddleware
+      env = instance.call({})
+      expect(env[:test]).to eq(2)
+    end
+  end
+end
