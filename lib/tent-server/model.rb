557e804e9ecb1b85b2740875aba230aadeaa210e
Jonathan Rudenberg <jonathan@titanous.com>
2012-08-25 14:11:24 -0400

    Add follow model

--- lib/tent-server/model.rb
+++ lib/tent-server/model.rb
@@ -4,5 +4,6 @@ require 'tent-server/data_mapper_array_property'
 module TentServer
   module Model
     autoload :Post, 'tent-server/model/post'
+    autoload :Follow, 'tent-server/model/follow'
   end
 end
---
+++ lib/tent-server/model/follow.rb
@@ -0,0 +1,15 @@
+module TentServer
+  module Model
+    class Follow
+      include DataMapper::Resource
+
+      storage_names[:default] = 'follows'
+
+      property :id, Serial
+      property :groups, Array
+      property :entity, URI
+      property :profile, Json
+      property :type, Enum[:following, :follower]
+    end
+  end
+end
