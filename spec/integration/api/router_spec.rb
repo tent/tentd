63133cb723c93ef17a9f71e528b2616a5dac2c1f
Jesse Stuart <jessestuart@gmail.com>
2012-08-25 11:41:40 -0400

    Add another TentServer::API::Router integration test

--- spec/integration/api/router_spec.rb
+++ spec/integration/api/router_spec.rb
@@ -1,31 +1,43 @@
 require 'spec_helper'
 
+class TestMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    env['response'] = { 'params' => env['params'] }
+    @app.call(env)
+  end
+end
+
+class TestApp
+  include TentServer::API::Router
+
+  get '/foo/:bar' do |b|
+    b.use TestMiddleware
+  end
+
+  post %r{^/foo/([^/]+)/bar} do |b|
+    b.use TestMiddleware
+  end
+end
+
 describe TentServer::API::Router do
-  class TestMiddleware
-    def initialize(app)
-      @app = app
-    end
-
-    def call(env)
-      env['response'] = { 'params' => env['params'] }
-      @app.call(env)
-    end
-  end
-
-  class TestApp
-    include TentServer::API::Router
-
-    get '/foo/:bar' do |b|
-      b.use TestMiddleware
-    end
-  end
-
   def app
     TestApp.new
   end
 
-  it "should work" do
+  it "should extract params" do
     get '/foo/baz'
     expect(JSON.parse(last_response.body)['params']['bar']).to eq('baz')
+  end
+
+  it "should merge both sets of params" do
+    post '/foo/baz/bar?chunky=bacon'
+    expect(last_response.status).to eq(200)
+    actual_body = JSON.parse(last_response.body)
+    expect(actual_body['params']['chunky']).to eq('bacon')
+    expect(actual_body['params']['captures']).to include('baz')
   end
 end
