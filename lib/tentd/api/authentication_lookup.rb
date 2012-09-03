b3417665506441c06210fb2a680f1197657cb626
Jesse Stuart <jessestuart@gmail.com>
2012-09-03 13:29:02 -0400

    Fix: Don't assume HTTP_AUTHORIZATION is hmac

--- lib/tentd/api/authentication_lookup.rb
+++ lib/tentd/api/authentication_lookup.rb
@@ -12,8 +12,10 @@ module TentD
         when 'u'
           env.potential_auth = TentD::Model::AppAuthorization.first(:mac_key_id => env['hmac']['id'])
         end
-        env.hmac.secret = env.potential_auth.mac_key
-        env.hmac.algorithm = env.potential_auth.mac_algorithm
+        if env.potential_auth
+          env.hmac.secret = env.potential_auth.mac_key
+          env.hmac.algorithm = env.potential_auth.mac_algorithm
+        end
         env
       end
     end
