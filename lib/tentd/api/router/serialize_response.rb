99ead5e702d8fc0bf36dc0abb1b4834a48d0f3f8
Jonathan Rudenberg <jonathan@titanous.com>
2012-09-05 18:19:51 -0400

    Don't set the Content-Type if the body is empty

--- lib/tentd/api/router/serialize_response.rb
+++ lib/tentd/api/router/serialize_response.rb
@@ -9,7 +9,11 @@ module TentD
             env.response.kind_of?(String) ? env.response : env.response.to_json(serialization_options(env))
           end
           status = env['response.status'] || (response ? 200 : 404)
-          headers = { 'Content-Type' => env['response.type'] || MEDIA_TYPE }
+          headers = if env['response.type'] || status == 200 && response && !response.empty?
+                      { 'Content-Type' => env['response.type'] || MEDIA_TYPE } 
+                    else
+                      {}
+                    end
           [status, headers, [response.to_s]]
         end
 
