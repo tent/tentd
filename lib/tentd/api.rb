f4a40b09739ca1a468216510c1b09f5e3c90ad85
Jonathan Rudenberg <jonathan@titanous.com>
2012-09-07 16:19:51 -0400

    Add support for CORS

--- lib/tentd/api.rb
+++ lib/tentd/api.rb
@@ -34,8 +34,24 @@ module TentD
       end
     end
 
+    class CorsPreflight < Middleware
+      def action(env)
+        headers = {
+          'Access-Control-Allow-Origin' => '*',
+          'Access-Control-Allow-Methods' => 'GET, POST, HEAD, PUT, DELETE, PATCH, OPTIONS',
+          'Access-Control-Allow-Headers' => 'Authorization',
+          'Access-Control-Max-Age' => '2592000' # 30 days
+        }
+        [200, headers, []]
+      end
+    end
+
     get '/' do |b|
       b.use HelloWorld
+    end
+
+    options %r{/.*} do |b|
+      b.use CorsPreflight
     end
   end
 end
