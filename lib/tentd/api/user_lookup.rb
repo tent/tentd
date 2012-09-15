70c713827e0160a7f156723ebe5e13d8ea32f1bd
Jonathan Rudenberg <jonathan@titanous.com>
2012-09-15 16:19:44 -0400

    Don't overwrite assigned user

--- lib/tentd/api/user_lookup.rb
+++ lib/tentd/api/user_lookup.rb
@@ -2,7 +2,7 @@ module TentD
   class API
     class UserLookup < Middleware
       def action(env)
-        Model::User.current = Model::User.first_or_create
+        Model::User.current ||= Model::User.first_or_create
         env
       end
     end
