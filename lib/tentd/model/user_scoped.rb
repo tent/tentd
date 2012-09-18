bebb59b479cac4758ac04a0d067d92bfd37cc140
Jonathan Rudenberg <jonathan@titanous.com>
2012-09-17 20:13:42 -0400

    Don't default the default_scope

--- lib/tentd/model/user_scoped.rb
+++ lib/tentd/model/user_scoped.rb
@@ -3,7 +3,6 @@ module TentD
     module UserScoped
       def self.included(base)
         base.class_eval do
-          default_scope(:default).update(:user => nil)
           belongs_to :user, 'TentD::Model::User'
           before :valid? do
             self.user_id ||= User.current.id
