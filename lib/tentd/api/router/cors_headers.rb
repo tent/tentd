f9d9368f7051de95d28760ca5e74f26b83751669
Jonathan Rudenberg <jonathan@titanous.com>
2012-09-23 19:35:30 -0400

    Remove unused conditional in CorsHeaders

--- lib/tentd/api/router/cors_headers.rb
+++ lib/tentd/api/router/cors_headers.rb
@@ -7,16 +7,6 @@ module TentD
         end
 
         def call(env)
-          if env['HTTP_METHOD'] == 'OPTIONS'
-            headers = {
-              'Access-Control-Allow-Origin' => '*',
-              'Access-Control-Allow-Methods' => 'GET, POST, HEAD, PUT, DELETE, PATCH, OPTIONS',
-              'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
-              'Access-Control-Max-Age' => '2592000' # 30 days
-            }
-            return [200, headers, []]
-          end
-
           status, headers, body = @app.call(env)
           headers.merge!(
             'Access-Control-Allow-Origin' => '*',
