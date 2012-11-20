8c4ee6ffdbad7cb100517ba262871cfa460b9368
Jesse Stuart <jessestuart@gmail.com>
2012-11-20 11:46:42 -0500

    follower spec passing

--- lib/tentd/model/follower.rb
+++ lib/tentd/model/follower.rb
@@ -6,6 +6,7 @@ module TentD
     class Follower < Sequel::Model(:followers)
       include RandomPublicId
       include Serializable
+      include Permissible
 
       plugin :serialization
       serialize_attributes :pg_array, :groups, :licenses
@@ -25,10 +26,88 @@ module TentD
         self.mac_key ||= SecureRandom.hex(16)
         self.mac_algorithm ||= 'hmac-sha-256'
         self.user_id ||= User.current.id
+        self.created_at = Time.now
+        super
+      end
+
+      def before_save
+        self.updated_at = Time.now
+        super
       end
 
       def permissible_foreign_key
         :follower_access_id
+      end
+
+      def self.public_attributes
+        [:entity]
+      end
+
+      def self.create_follower(data, authorized_scopes = [])
+        existing_followers = where(:entity => data.entity)
+        if existing_followers.any?
+          existing_followers.destroy
+        end
+
+        if authorized_scopes.include?(:write_followers) && authorized_scopes.include?(:write_secrets)
+          follower = create(data.slice(:public_id, :entity, :groups, :public, :profile, :licenses, :notification_path, :mac_key_id, :mac_key, :mac_algorithm, :mac_timestamp_delta))
+          if data.permissions
+            follower.assign_permissions(data.permissions)
+          end
+        else
+          follower = create(data.slice('entity', 'licenses', 'profile', 'notification_path'))
+        end
+        (data.types || ['all']).each do |type_url|
+          NotificationSubscription.create(
+            :follower => follower,
+            :type => type_url
+          )
+        end
+        follower
+      end
+
+      def self.update_entity(follower_id)
+        first(:id => follower_id).update_entity
+      end
+
+      def update_entity
+        client = TentClient.new(core_profile.servers, auth_details.merge(:faraday_adapter => TentD.faraday_adapter))
+        res = client.profile.get
+        old_entity = self.entity
+        if res.status == 200
+          self.profile = res.body
+          self.licenses = core_profile.licenses
+          self.entity = core_profile.entity
+          save
+        end
+        propagate_entity(self.entity, old_entity) if self.entity != old_entity
+        profile
+      end
+
+      def propagate_entity(new_entity, old_entity)
+        Post.where(:entity => old_entity, :original => false).update(:entity => entity)
+        Mention.where(:entity => old_entity, :original_post => false).update(:entity => entity)
+      end
+
+      def auth_details
+        attributes.slice(:mac_key_id, :mac_key, :mac_algorithm)
+      end
+
+      def core_profile
+        API::CoreProfileData.new(profile)
+      end
+
+      def as_json(options = {})
+        attributes = super
+
+        attributes.merge!(:profile => profile) if options[:app]
+
+        if options[:app] || options[:self]
+          types = notification_subscriptions.map { |s| s.type.uri }
+          attributes.merge!(:licenses => licenses, :types => types, :notification_path => notification_path)
+        end
+
+        attributes
       end
     end
   end
@@ -68,25 +147,6 @@ end
 #       # permissions describing what they have access to
 #       has n, :access_permissions, 'TentD::Model::Permission', :child_key => [ :follower_access_id ], :constraint => :destroy
 #
-#       def self.create_follower(data, authorized_scopes = [])
-#         if existing_followers = all(:entity => data.entity)
-#           existing_followers.destroy
-#         end
-#
-#         if authorized_scopes.include?(:write_followers) && authorized_scopes.include?(:write_secrets)
-#           follower = create(data.slice(:public_id, :entity, :groups, :public, :profile, :licenses, :notification_path, :mac_key_id, :mac_key, :mac_algorithm, :mac_timestamp_delta))
-#           if data.permissions
-#             follower.assign_permissions(data.permissions, :visibility_permissions)
-#           end
-#         else
-#           follower = create(data.slice('entity', 'licenses', 'profile', 'notification_path'))
-#         end
-#         (data.types || ['all']).each do |type_url|
-#           follower.notification_subscriptions.create(:type => type_url)
-#         end
-#         follower
-#       end
-#
 #       def self.update_follower(id, data, authorized_scopes = [])
 #         follower = first(:id => id)
 #         return unless follower
@@ -108,56 +168,8 @@ end
 #         follower
 #       end
 #
-#       def self.update_entity(follower_id)
-#         first(:id => follower_id).update_entity
-#       end
-#
-#       def self.public_attributes
-#         [:entity]
-#       end
-#
-#       def update_entity
-#         client = TentClient.new(core_profile.servers, auth_details.merge(:faraday_adapter => TentD.faraday_adapter))
-#         res = client.profile.get
-#         old_entity = self.entity
-#         if res.status == 200
-#           self.profile = res.body
-#           self.licenses = core_profile.licenses
-#           self.entity = core_profile.entity
-#           save
-#         end
-#         propagate_entity(self.entity, old_entity) if self.entity != old_entity
-#         profile
-#       end
-#
-#       def propagate_entity(new_entity, old_entity)
-#         Post.all(:entity => old_entity, :original => false).update(:entity => entity)
-#         Mention.all(:entity => old_entity, :original_post => false).update(:entity => entity)
-#       end
-#
-#       def core_profile
-#         API::CoreProfileData.new(profile)
-#       end
-#
 #       def notification_servers
 #         core_profile.servers
-#       end
-#
-#       def auth_details
-#         attributes.slice(:mac_key_id, :mac_key, :mac_algorithm)
-#       end
-#
-#       def as_json(options = {})
-#         attributes = super
-#
-#         attributes.merge!(:profile => profile) if options[:app]
-#
-#         if options[:app] || options[:self]
-#           types = notification_subscriptions.all.map { |s| s.type.uri }
-#           attributes.merge!(:licenses => licenses, :types => types, :notification_path => notification_path)
-#         end
-#
-#         attributes
 #       end
 #     end
 #   end
