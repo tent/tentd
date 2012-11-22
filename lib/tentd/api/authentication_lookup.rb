7d924a17be6bcf0fe680abbd353c3bbbab199d8e
Jesse Stuart <jessestuart@gmail.com>
2012-11-22 12:44:32 -0500

    User user scoping for authentication lookup

--- lib/tentd/api/authentication_lookup.rb
+++ lib/tentd/api/authentication_lookup.rb
@@ -7,9 +7,9 @@ module TentD
         mac_key_id = env.hmac.id
         env.potential_auth = case mac_key_id.to_s[0,1]
         when 's'
-          TentD::Model::Follower.first(:mac_key_id => mac_key_id)
+          TentD::Model::Follower.first(:user_id => TentD::Model::User.current.id, :mac_key_id => mac_key_id)
         when 'a'
-          TentD::Model::App.first(:mac_key_id => mac_key_id)
+          TentD::Model::App.first(:user_id => TentD::Model::User.current.id, :mac_key_id => mac_key_id)
         when 'u'
           TentD::Model::AppAuthorization.qualify.join(
             :apps,
@@ -20,7 +20,7 @@ module TentD
             :apps__deleted_at => nil
           ).first
         end
-        env.potential_auth = Model::Following.first(:mac_key_id => mac_key_id) unless env.potential_auth
+        env.potential_auth = Model::Following.first(:user_id => TentD::Model::User.current.id, :mac_key_id => mac_key_id) unless env.potential_auth
         if env.potential_auth
           env.hmac.secret = env.potential_auth.mac_key
           env.hmac.algorithm = env.potential_auth.mac_algorithm
