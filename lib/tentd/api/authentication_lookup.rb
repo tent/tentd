5a7590b7f49bc86cff59a33fdb0ca9d7219cf966
Jesse Stuart <jesse@jessestuart.ca>
2013-02-14 11:30:02 -0500

    Fix: Don't attempt to authenticate following with nil mac_key_id

--- lib/tentd/api/authentication_lookup.rb
+++ lib/tentd/api/authentication_lookup.rb
@@ -20,7 +20,7 @@ module TentD
             :apps__deleted_at => nil
           ).where("app_authorizations.expires_at IS NULL OR app_authorizations.expires_at > ?", Time.now.to_i).first
         end
-        env.potential_auth = Model::Following.first(:user_id => TentD::Model::User.current.id, :mac_key_id => mac_key_id) unless env.potential_auth
+        env.potential_auth = Model::Following.first(:user_id => TentD::Model::User.current.id, :mac_key_id => mac_key_id) if !env.potential_auth && mac_key_id
         if env.potential_auth
           env.hmac.secret = env.potential_auth.mac_key
           env.hmac.algorithm = env.potential_auth.mac_algorithm
