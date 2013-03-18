07c36338078134658a3e6821b621249fd3b8cd95
Jesse Stuart <jesse@jessestuart.ca>
2013-03-18 14:18:00 -0400

    Don't use autoload

--- lib/tentd.rb
+++ lib/tentd.rb
@@ -3,26 +3,19 @@ require 'sequel'
 require 'logger'
 
 module TentD
-  autoload :Model, 'tentd/model'
 
   TENT_VERSION = '0.3'.freeze
 
-  def self.new(options = {})
+  def self.setup!(options = {})
     if database_url = options[:database] || ENV['DATABASE_URL']
-      self.database_url = database_url
+      @database = Sequel.connect(database_url, :logger => Logger.new(ENV['DB_LOGFILE'] || STDOUT))
     end
+
+    require 'tentd/model'
   end
 
   def self.database
-    @database || begin
-      return unless database_url = ENV['DATABASE_URL']
-      self.database_url = database_url
-      @database
-    end
-  end
-
-  def self.database_url=(database_url)
-    @database = Sequel.connect(database_url, :logger => Logger.new(ENV['DB_LOGFILE'] || STDOUT))
+    @database
   end
 
 end
