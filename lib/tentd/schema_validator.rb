e6eca4d9138d5204547e6b72ccc9c354f73b67c3
Jesse Stuart <jesse@jessestuart.ca>
2013-04-04 20:47:37 -0400

    Discard null post members

--- lib/tentd/schema_validator.rb
+++ lib/tentd/schema_validator.rb
@@ -13,6 +13,8 @@ module TentD
       type = TentClient::TentType.new(type_uri)
       schema_name = "post_#{type.base.split('/').last}"
 
+      remove_null_members(data)
+
       # Validate post schema
       schema = @schemas['post'].dup
       schema['properties'].each_pair { |name, property| property['required'] = false }
@@ -27,6 +29,24 @@ module TentD
 
     def valid?(data)
       diff(data, failed_assertions(data)).empty?
+    end
+
+    private
+
+    def self.remove_null_members(hash)
+      hash.each_pair do |key, val|
+        case val
+        when Hash
+          remove_null_members(val)
+        when Array
+          val.each do |item|
+            next unless Hash === item
+            remove_null_members(item)
+          end
+        when NilClass
+          hash.delete(key)
+        end
+      end
     end
 
   end
