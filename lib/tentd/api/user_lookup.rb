b2734029b71dc254340dcb815d9e52d2d3673d63
Jesse Stuart <jesse@jessestuart.ca>
2013-04-08 13:11:45 -0400

    Allow setting env['current_user'] before UserLookup

--- lib/tentd/api/user_lookup.rb
+++ lib/tentd/api/user_lookup.rb
@@ -5,8 +5,10 @@ module TentD
       EntityNotSetError = Class.new(StandardError)
 
       def action(env)
-        raise EntityNotSetError.new("You need to set ENV['TENT_ENTITY']!") unless ENV['TENT_ENTITY']
-        env['current_user'] = Model::User.first_or_create(ENV['TENT_ENTITY'])
+        unless env['current_user']
+          raise EntityNotSetError.new("You need to set ENV['TENT_ENTITY']!") unless ENV['TENT_ENTITY']
+          env['current_user'] = Model::User.first_or_create(ENV['TENT_ENTITY'])
+        end
         env
       end
     end
