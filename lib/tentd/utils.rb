d2431cc88121cf8e40eca3b8fc898d0c71e8018d
Jesse Stuart <jesse@jessestuart.ca>
2013-04-15 13:03:43 -0400

    Add symbolize_keys Hash helper

--- lib/tentd/utils.rb
+++ lib/tentd/utils.rb
@@ -48,27 +48,35 @@ module TentD
       end
 
       def stringify_keys(hash)
-        _stringify_keys(hash).first
-      end
-
-      def _stringify_keys(*items)
-        items.map do |item|
-          case item
-          when ::Hash
-            item.inject(::Hash.new) do |new_hash, (k,v)|
-              new_hash[k.to_s] = _stringify_keys(v).first
-              new_hash
-            end
-          when ::Array
-            item.map { |i| _stringify_keys(i).first }
-          else
-            item
-          end
-        end
+        transform_keys(hash, :to_s).first
       end
 
       def stringify_keys!(hash)
         hash.replace(stringify_keys(hash))
+      end
+
+      def symbolize_keys(hash)
+        transform_keys(hash, :to_sym).first
+      end
+
+      def symbolize_keys!(hash)
+        hash.replace(symbolize_keys(hash))
+      end
+
+      def transform_keys(*items, method)
+        items.map do |item|
+          case item
+          when ::Hash
+            item.inject(::Hash.new) do |new_hash, (k,v)|
+              new_hash[k.send(method)] = transform_keys(v, method).first
+              new_hash
+            end
+          when ::Array
+            item.map { |i| transform_keys(i, method).first }
+          else
+            item
+          end
+        end
       end
     end
 
