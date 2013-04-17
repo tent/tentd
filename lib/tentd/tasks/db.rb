0fae255b881f3b9ada1aefdc3b424683b1d0155d
Jesse Stuart <jesse@jessestuart.ca>
2013-04-17 16:31:19 -0400

    Add rake tasks for creating and dropping db

--- lib/tentd/tasks/db.rb
+++ lib/tentd/tasks/db.rb
@@ -1,6 +1,30 @@
+PG_DB_URL_REGEXP = %r{\A(?:postgres://(?:([^:]+):)?(?:([^@]+)@)?([^/]+)/)?(.+)\Z}.freeze
+
 namespace :db do
   task :migrate do
     path = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'db', 'migrations'))
     %x{bundle exec sequel -m #{path} #{ENV['DATABASE_URL']}}
+  end
+
+  task :create do
+    exit 1 unless m = ENV['DATABASE_URL'].to_s.match(PG_DB_URL_REGEXP)
+    opts = {
+      :username => m[1],
+      :host => m[3],
+    }.inject([]) { |m, (k,v)| next m unless v; m << %(--#{k}="#{v}"); m }.join(" ")
+    dbname = m[4]
+
+    %x{createdb #{opts} #{dbname}}
+  end
+
+  task :drop do
+    exit 1 unless m = ENV['DATABASE_URL'].to_s.match(PG_DB_URL_REGEXP)
+    opts = {
+      :username => m[1],
+      :host => m[3],
+    }.inject([]) { |m, (k,v)| next m unless v; m << %(--#{k}="#{v}"); m }.join(" ")
+    dbname = m[4]
+
+    %x{dropdb #{opts} #{dbname}}
   end
 end
