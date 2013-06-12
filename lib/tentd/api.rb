de01a580a93464f8bc4ce78f6852301d4d74ceb6
Jesse Stuart <jesse@jessestuart.ca>
2013-06-12 13:49:56 -0400

    Add GET post versions support

--- lib/tentd/api.rb
+++ lib/tentd/api.rb
@@ -13,6 +13,7 @@ module TentD
 
     MENTIONS_CONTENT_TYPE = %(application/vnd.tent.post-mentions.v0+json).freeze
     CHILDREN_CONTENT_TYPE = %(application/vnd.tent.post-children.v0+json).freeze
+    VERSIONS_CONTENT_TYPE = %(application/vnd.tent.post-versions.v0+json).freeze
 
     require 'tentd/api/serialize_response'
     require 'tentd/api/middleware'
@@ -161,6 +162,65 @@ module TentD
       end
     end
 
+    class ListPostVersions < Middleware
+      def action(env)
+        ref_post = env.delete('response.post')
+
+        q = Query.new(Model::Post)
+
+        q.query_conditions << "posts.user_id = ?"
+        q.query_bindings << env['current_user'].id
+
+        q.query_conditions << "posts.public_id = ?"
+        q.query_bindings << ref_post.public_id
+
+        authorizer = Authorizer.new(env)
+        if env['current_auth'] && authorizer.auth_candidate
+          unless authorizer.auth_candidate.read_all_types?
+            _read_type_ids = Model::Type.find_types(authorizer.auth_candidate.read_types).inject({:base => [], :full => []}) do |memo, type|
+              if type.fragment.nil?
+                memo[:base] << type.id
+              else
+                memo[:full] << type.id
+              end
+              memo
+            end
+
+            q.query_conditions << ["OR",
+              "posts.public = true",
+              ["AND",
+                "posts.entity_id = ?",
+                ["OR",
+                  "posts.type_base_id IN ?",
+                  "posts.type_id IN ?"
+                ]
+              ]
+            ]
+            q.query_bindings << env['current_user'].entity_id
+            q.query_bindings << _read_type_ids[:base]
+            q.query_bindings << _read_type_ids[:full]
+          end
+        else
+          q.query_conditions << "posts.public = true"
+        end
+
+        q.sort_columns = ["posts.version_received_at DESC"]
+
+        q.limit = Feed::DEFAULT_PAGE_LIMIT
+
+        versions = q.all
+
+        env['response'] = {
+          :versions => versions.map { |post| post.version_as_json(:env => env).merge(:type => post.type) }
+        }
+
+        env['response.headers'] = {}
+        env['response.headers']['Content-Type'] = VERSIONS_CONTENT_TYPE
+
+        env
+      end
+    end
+
     class GetPost < Middleware
       def action(env)
         params = env['params']
@@ -178,6 +238,8 @@ module TentD
           return ListPostMentions.new(@app).call(env)
         when CHILDREN_CONTENT_TYPE
           return ListPostChildren.new(@app).call(env)
+        when VERSIONS_CONTENT_TYPE
+          return ListPostVersions.new(@app).call(env)
         end
 
         env
