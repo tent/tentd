d9cafa4874ab45fd0d9d4aaab6e930d51d648726
Jesse Stuart <jesse@jessestuart.ca>
2013-06-13 14:43:22 -0400

    Add uri-tempalte format validator

--- lib/tentd/schema_validator/format_validators.rb
+++ lib/tentd/schema_validator/format_validators.rb
@@ -12,3 +12,13 @@ ApiValidator.format_validators['https://tent.io/formats/page-uri'] = lambda do |
   # see pchar format in https://tools.ietf.org/html/rfc3986#appendix-A
   value && value =~ /\A\?[-~._,:@%!&"()*+,;=a-z0-9]{0,}\Z/i
 end
+
+ApiValidator.format_validators['uri-template'] = lambda do |value|
+  return false unless String === value
+  begin
+    uri = URI(value.gsub(/[{}]/, ''))
+    uri.scheme && uri.host
+  rescue URI::InvalidURIError, ArgumentError
+    false
+  end
+end
