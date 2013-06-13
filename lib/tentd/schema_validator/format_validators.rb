c70177450b40da6ae651c5d909944d23b2a320cb
Jesse Stuart <jesse@jessestuart.ca>
2013-06-13 14:43:10 -0400

    Fix typeo

--- lib/tentd/schema_validator/format_validators.rb
+++ lib/tentd/schema_validator/format_validators.rb
@@ -1,7 +1,7 @@
 ApiValidator.format_validators['https://tent.io/formats/authorize-type-uri'] = lambda do |value|
   return true if value == 'all'
   begin
-    uri = URI(actual)
+    uri = URI(value)
     uri.scheme && uri.host
   rescue URI::InvalidURIError, ArgumentError
     false
