17048dff053b70088e1f8e23bb75e82b6eaaa026
Jesse Stuart <jesse@jessestuart.ca>
2013-06-15 22:58:27 -0400

    Fix race condition for Entity.first_or_create

--- lib/tentd/models/entity.rb
+++ lib/tentd/models/entity.rb
@@ -5,6 +5,8 @@ module TentD
 
       def self.first_or_create(entity_uri)
         first(:entity => entity_uri) || create(:entity => entity_uri)
+      rescue Sequel::UniqueConstraintViolation
+        first(:entity => entity_uri)
       end
 
     end
