0c78ce9a9818ceef865aba2c68f50238c5d50dec
Jesse Stuart <jesse@jessestuart.ca>
2013-06-20 15:40:03 -0400

    Patch PostsAttachments.create to allow attempting to create duplicates without error being raised

--- lib/tentd/models/posts_attachment.rb
+++ lib/tentd/models/posts_attachment.rb
@@ -3,6 +3,18 @@ module TentD
 
     class PostsAttachment < Sequel::Model(TentD.database[:posts_attachments])
       plugin :paranoia if Model.soft_delete
+
+
+      def self.create(attrs)
+        super
+      rescue Sequel::UniqueConstraintViolation => e
+        if e.message =~ /duplicate key.*unique_posts_attachments/
+          attrs = Utils::Hash.symbolize_keys(attrs)
+          first(:post_id => attrs[:post_id], :attachment_id => attrs[:attachment_id], :content_type => attrs[:content_type])
+        else
+          raise
+        end
+      end
     end
 
   end
