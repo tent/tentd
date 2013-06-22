c12e3ec70ac3fcabcb0abdb761ab201653aeecb7
Jesse Stuart <jesse@jessestuart.ca>
2013-06-22 12:26:26 -0400

    Add request proxy support for GET post mentions/versions/children

--- lib/tentd/api.rb
+++ lib/tentd/api.rb
@@ -81,6 +81,33 @@ module TentD
       end
     end
 
+    class ProxyPostList < Middleware
+      def action(env)
+        unless [MENTIONS_CONTENT_TYPE, CHILDREN_CONTENT_TYPE, VERSIONS_CONTENT_TYPE].include?(env['HTTP_ACCEPT'])
+          return env
+        end
+
+        params = env['params']
+        request_proxy_manager = env['request_proxy_manager']
+
+        return env if request_proxy_manager.proxy_condition == :never
+
+        proxy_client = request_proxy_manager.proxy_client(params[:entity], :skip_response_serialization => true)
+
+        _params = Utils::Hash.slice(params, :limit)
+        res = proxy_client.post.get(params[:entity], params[:post], _params) do |request|
+          request.headers['Accept'] = env['HTTP_ACCEPT']
+        end
+
+        body = res.body.respond_to?(:each) ? res.body : [res.body]
+        return [res.status, res.headers, body]
+      rescue Faraday::Error::TimeoutError
+        halt!(504, "Failed to proxy request: #{res.env[:method].to_s.upcase} #{res.env[:url].to_s}")
+      rescue Faraday::Error::ConnectionFailed
+        halt!(502, "Failed to proxy request: #{res.env[:method].to_s.upcase} #{res.env[:url].to_s}")
+      end
+    end
+
     class ListPostMentions < Middleware
       def action(env)
         ref_post = env.delete('response.post')
@@ -610,6 +637,7 @@ module TentD
 
     get '/posts/:entity/:post' do |b|
       b.use AuthorizeGetEntity
+      b.use ProxyPostList
       b.use LookupPost
       b.use GetPost
       b.use ServePost
